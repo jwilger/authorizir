90e9c012b17d6782f55682fc4b99763316e61445
John Wilger <john@johnwilger.com>
2022-01-25 18:02:54 -0800

    Tests currently passing, but likely changing direction

--- lib/authorizir.ex
+++ lib/authorizir.ex
@@ -1,14 +1,169 @@
 defmodule Authorizir do
-  @moduledoc """
+  @moduledoc ~S"""
   Ecto-backed Authorization Library for Elixir Applications
 
   See [README](README.md) for a description of the mathematical model used as
   the basis of this system.
+
+  ## Usage
+
+  Imagine you are creating an app that handles online ordering.
+
+  First, create your app's authorization module, configuring it with your
+  application repository:
+
+  ```elixir
+  defmodule Auth do
+    use Authorizir, repo: Repo
+  end
+  ```
+
+  Users of the application might be organized into a hierarchy as follows (note
+  that an employee can also be a customer):
+
+  ```mermaid
+  graph TD
+    *[Users *] --> E[Employees]
+    * --> C[Customers]
+
+    E --> A[Admins]
+    E --> M[Marketing]
+    E --> F[Finance]
+    E --> S[Shipping and Fulfillment]
+
+    A --> Bob
+
+    M --> Bob
+    M --> Jane
+
+    F --> Amanda
+
+    S --> George
+    S --> Beth
+
+    C --> Amanda
+    C --> George
+    C --> John
+  ```
+
+  We have two types of Subject entities represented here; "Organizational Units"
+  represent groups of users such as internal departments and customers, while
+  "Users" represent the individual system accounts. Each of these are
+  represented with Ecto schemas in our app, and we include the
+  `Authorizir.Subject` behavior in the modules, so that they can participate in
+  the Subject hierarcy.
+
+  First we add the necessary migrations by running `mix ecto.gen.migraion
+  add_org_units_and_users` and editing the resulting migration file:
+
+  ```elixir
+  defmodule AddOrgUnitsAndUsers do
+    use Ecto.Migration
+    import Authorizir.Migrations, only: [apply_subject_hierarchy: 2]
+
+    create table("org_units") do
+      add :name, :string, null: false
+      timestamps()
+    end
+    apply_subject_hierarchy("org_units", id_field: :id)
+
+    create table("org_units") do
+      add :name, :string, null: false
+      timestamps()
+    end
+    apply_subject_hierarchy("org_units", id_field: :id)
+  end
+  ```
+
+  ```elixir
+  defmodule OrgUnit do
+    use Ecto.Schema
+    use Authorizir.Subject
+
+    schema "org_units" do
+      field :name, :string
+    end
+  end
+
+  defmodule User do
+    use Ecto.Schema
+    use Authorizir.Subject
+
+    schema "org_units" do
+      field :name, :string
+    end
+  end
+  ```
+
+  You can create the hierarchy as:
+
+  ```elixir
+  {:ok, employees} = %OrgUnit{name: "Employees"} |> Repo.insert()
+
+  {:ok, customers} = %OrgUnit{name: "Customers"} |> Repo.insert()
+
+  {:ok, admins} = %OrgUnit{name: "Admins"} |> Repo.insert()
+  :ok = Auth.add_child(employees.id, admins.id, Subject)
+
+  {:ok, marketing} = %OrgUnit{name: "Marketing"} |> Repo.insert()
+  :ok = Auth.add_child(employees.id, marketing.id, Subject)
+
+  {:ok, finance} = %OrgUnit{name: "Finance"} |> Repo.insert()
+  :ok = Auth.add_child(employees.id, finance.id, Subject)
+
+  {:ok, shipping} = %OrgUnit{name: "Shipping and Fulfillment"} |> Repo.insert()
+  :ok = Auth.add_child(employees.id, shipping.id, Subject)
+
+  {:ok, bob} = %User{name: "Bob"} |> Repo.insert()
+  :ok = Auth.add_child(admins.id, bob.id, Subject)
+  :ok = Auth.add_child(marketing.id, bob.id, Subject)
+
+  {:ok, jane} = %User{name: "Jane"} |> Repo.insert()
+  :ok = Auth.add_child(marketing.id, jane.id, Subject)
+
+  {:ok, amanda} = %User{name: "Amanda"} |> Repo.insert()
+  :ok = Auth.add_child(finance.id, amanda.id, Subject)
+  :ok = Auth.add_child(customers.id, amanda.id, Subject)
+
+  {:ok, george} = %User{name: "George"} |> Repo.insert()
+  :ok = Auth.add_child(shipping.id, george.id, Subject)
+  :ok = Auth.add_child(customers.id, george.id, Subject)
+
+  {:ok, beth} = %User{name: "Beth"} |> Repo.insert()
+  :ok = Auth.add_child(shipping.id, beth.id, Subject)
+
+  {:ok, john} = %User{name: "John"} |> Repo.insert()
+  :ok = Auth.add_child(customers.id, john.id, Subject)
+  ```
+
   """
 
   alias Authorizir.{AuthorizationRule, Object, Permission, Subject}
 
+  import Authorizir.ErrorHelpers, only: [errors_on: 2]
   import Ecto.Query, only: [from: 2]
+
+  @callback register_subject(id :: binary(), description :: String.t()) ::
+              :ok | {:error, reason :: atom()}
+
+  def register_subject(repo, id, description) do
+    case Subject.new(id, description) |> repo.insert() do
+      {:ok, _subject} ->
+        :ok
+
+      {:error, changeset} ->
+        cond do
+          "can't be blank" in errors_on(changeset, :ext_id) ->
+            {:error, :id_is_required}
+
+          "can't be blank" in errors_on(changeset, :description) ->
+            {:error, :description_is_required}
+
+          true ->
+            raise "Unanticipated error while adding Subject: #{inspect(changeset)}"
+        end
+    end
+  end
 
   @callback grant_permission(
               subject_id :: binary(),
@@ -50,14 +205,99 @@ defmodule Authorizir do
     delete_rule(repo, subject_id, object_id, permission_id, :-)
   end
 
+  @callback add_child(parent_id :: binary(), child_id :: binary(), type :: module()) ::
+              :ok | {:error, reason :: atom()}
+
+  def add_child(repo, parent_id, child_id, type) do
+    with {:parent, parent} when not is_nil(parent) <-
+           {:parent, repo.get_by(type, ext_id: parent_id)},
+         {:child, child} when not is_nil(child) <- {:child, repo.get_by(type, ext_id: child_id)},
+         {:edge_created, _edge} <- type.create_edge(parent, child) |> repo.dagex_update() do
+      :ok
+    else
+      {:parent, nil} -> {:error, :invalid_parent}
+      {:child, nil} -> {:error, :invalid_parent}
+      {:error, _reason} = error -> error
+    end
+  end
+
+  @callback remove_child(parent_id :: binary(), child_id :: binary(), type :: module()) ::
+              :ok | {:error, reason :: atom()}
+
+  def remove_child(repo, parent_id, child_id, type) do
+    with {:parent, parent} when not is_nil(parent) <-
+           {:parent, repo.get_by(type, ext_id: parent_id)},
+         {:child, child} when not is_nil(child) <- {:child, repo.get_by(type, ext_id: child_id)},
+         {:edge_removed, _edge} <- type.remove_edge(parent, child) |> repo.dagex_update() do
+      :ok
+    else
+      {:parent, nil} -> {:error, :invalid_parent}
+      {:child, nil} -> {:error, :invalid_parent}
+      {:error, _reason} = error -> error
+    end
+  end
+
+  @callback permission_granted?(
+              subject_id :: binary(),
+              object_id :: binary(),
+              permission_id :: binary()
+            ) :: :granted | :denied | {:error, reason :: atom()}
+
+  def permission_granted?(repo, subject_id, object_id, permission_id) do
+    with {:sop, {:ok, subject, object, permission}} <-
+           {:sop, sop_nodes(repo, subject_id, object_id, permission_id)} do
+      cond do
+        authorization_rule_applies?(repo, subject, object, permission, :-) -> :denied
+        authorization_rule_applies?(repo, subject, object, permission, :+) -> :granted
+        true -> :denied
+      end
+    else
+      {:sop, error} -> error
+    end
+  end
+
+  defp authorization_rule_applies?(repo, subject, object, permission, :-) do
+    from([r, s, o] in authorization_rules_for(subject, object),
+      join: p in subquery(Permission.with_descendants(permission)),
+      on: p.id == r.permission_id,
+      where: r.rule_type == :-
+    )
+    |> repo.exists?()
+  end
+
+  defp authorization_rule_applies?(repo, subject, object, permission, :+) do
+    from([r, s, o] in authorization_rules_for(subject, object),
+      join: p in subquery(Permission.with_ancestors(permission)),
+      on: p.id == r.permission_id,
+      where: r.rule_type == :+
+    )
+    |> repo.exists?()
+  end
+
+  defp authorization_rules_for(subject, object) do
+    from(r in AuthorizationRule,
+      join: s in subquery(Subject.with_ancestors(subject)),
+      on: s.id == r.subject_id,
+      join: o in subquery(Object.with_ancestors(object)),
+      on: o.id == r.object_id
+    )
+  end
+
   defp sop_ids(repo, subject_ext_id, object_ext_id, permission_ext_id) do
-    with {:subject, %{id: subject_id}} <-
+    case sop_nodes(repo, subject_ext_id, object_ext_id, permission_ext_id) do
+      {:ok, subject, object, permission} -> {:ok, subject.id, object.id, permission.id}
+      result -> result
+    end
+  end
+
+  defp sop_nodes(repo, subject_ext_id, object_ext_id, permission_ext_id) do
+    with {:subject, %{} = subject} <-
            {:subject, repo.get_by(Subject, ext_id: subject_ext_id)},
-         {:object, %{id: object_id}} <-
+         {:object, %{} = object} <-
            {:object, repo.get_by(Object, ext_id: object_ext_id)},
-         {:permission, %{id: permission_id}} <-
+         {:permission, %{} = permission} <-
            {:permission, repo.get_by(Permission, ext_id: permission_ext_id)} do
-      {:ok, subject_id, object_id, permission_id}
+      {:ok, subject, object, permission}
     else
       {participant, nil} -> {:error, "invalid_#{participant}" |> String.to_atom()}
     end
@@ -129,6 +369,22 @@ defmodule Authorizir do
       @impl Authorizir
       def allow_permission(subject_id, object_id, permission_id),
         do: Authorizir.allow_permission(@authorizir_repo, subject_id, object_id, permission_id)
+
+      @impl Authorizir
+      def permission_granted?(subject_id, object_id, permission_id),
+        do: Authorizir.permission_granted?(@authorizir_repo, subject_id, object_id, permission_id)
+
+      @impl Authorizir
+      def add_child(parent_id, child_id, type),
+        do: Authorizir.add_child(@authorizir_repo, parent_id, child_id, type)
+
+      @impl Authorizir
+      def remove_child(parent_id, child_id, type),
+        do: Authorizir.remove_child(@authorizir_repo, parent_id, child_id, type)
+
+      @impl Authorizir
+      def register_subject(id, description),
+        do: Authorizir.register_subject(@authorizir_repo, id, description)
     end
   end
 end
--- test/authorizir_test.exs
+++ test/authorizir_test.exs
@@ -13,6 +13,24 @@ defmodule AuthorizirTest do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(AuthorizirTest.Repo)
   end
 
+  describe "register_subject/2" do
+    test "returns :ok if subject was successfully registered" do
+      :ok = Auth.register_subject(UUID.generate(), "some description")
+    end
+
+    test "returns an error if id is nil or blank" do
+      for id <- [nil, "", " "] do
+        {:error, :id_is_required} = Auth.register_subject(id, "some description")
+      end
+    end
+
+    test "returns an error if description is nil or blank" do
+      for desc <- [nil, "", " "] do
+        {:error, :description_is_required} = Auth.register_subject(UUID.generate(), desc)
+      end
+    end
+  end
+
   describe "grant_permission/3" do
     test "returns :ok if permission grant is successful" do
       {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
@@ -208,6 +226,317 @@ defmodule AuthorizirTest do
 
       {:error, :invalid_permission} =
         Auth.allow_permission(subject.ext_id, object.ext_id, UUID.generate())
+    end
+  end
+
+  describe "add_child/3" do
+    test "returns {:error, :invalid_parent} when parent id not registered as specified type" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+
+      {:error, :invalid_parent} = Auth.add_child(UUID.generate(), subject.ext_id, Subject)
+    end
+
+    test "returns {:error, :invalid_child} when child id not registered as specified type" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+
+      {:error, :invalid_parent} = Auth.add_child(subject.ext_id, UUID.generate(), Subject)
+    end
+
+    test "returns :ok when relationship is created" do
+      {:ok, subject_a} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject_b} = Subject.new(UUID.generate(), "Subject B") |> Repo.insert()
+
+      :ok = Auth.add_child(subject_a.ext_id, subject_b.ext_id, Subject)
+    end
+
+    test "creates Dagex parent/child association" do
+      {:ok, subject_a} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject_b} = Subject.new(UUID.generate(), "Subject B") |> Repo.insert()
+
+      :ok = Auth.add_child(subject_a.ext_id, subject_b.ext_id, Subject)
+
+      assert subject_a in (Subject.parents(subject_b) |> Repo.all())
+    end
+
+    test "returns Dagex error if edge cannot be created" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+
+      {:error, :cyclic_edge} = Auth.add_child(subject.ext_id, subject.ext_id, Subject)
+    end
+  end
+
+  describe "remove_child/3" do
+    test "returns {:error, :invalid_parent} when parent id not registered as specified type" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+
+      {:error, :invalid_parent} = Auth.remove_child(UUID.generate(), subject.ext_id, Subject)
+    end
+
+    test "returns {:error, :invalid_child} when child id not registered as specified type" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+
+      {:error, :invalid_parent} = Auth.remove_child(subject.ext_id, UUID.generate(), Subject)
+    end
+
+    test "returns :ok when relationship is removed" do
+      {:ok, subject_a} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject_b} = Subject.new(UUID.generate(), "Subject B") |> Repo.insert()
+
+      :ok = Auth.add_child(subject_a.ext_id, subject_b.ext_id, Subject)
+      :ok = Auth.remove_child(subject_a.ext_id, subject_b.ext_id, Subject)
+    end
+
+    test "removes Dagex parent/child association" do
+      {:ok, subject_a} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject_b} = Subject.new(UUID.generate(), "Subject B") |> Repo.insert()
+
+      :ok = Auth.add_child(subject_a.ext_id, subject_b.ext_id, Subject)
+      :ok = Auth.remove_child(subject_a.ext_id, subject_b.ext_id, Subject)
+
+      refute subject_a in (Subject.parents(subject_b) |> Repo.all())
+    end
+  end
+
+  describe "permission_granted?/3" do
+    test "returns {:error, :invalid_subject} if subject ID has not been registered" do
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      {:error, :invalid_subject} =
+        Auth.permission_granted?(UUID.generate(), object.ext_id, permission.ext_id)
+    end
+
+    test "returns {:error, :invalid_object} if object ID has not been registered" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      {:error, :invalid_object} =
+        Auth.permission_granted?(subject.ext_id, UUID.generate(), permission.ext_id)
+    end
+
+    test "returns {:error, :invalid_permission} if permission ID has not been registered" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+
+      {:error, :invalid_permission} =
+        Auth.permission_granted?(subject.ext_id, object.ext_id, UUID.generate())
+    end
+
+    test "returns :denied when no authorization rules exist that can affect the given sop" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :denied = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :denied when negative grant rule is applied to the sop" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.deny_permission(subject.ext_id, object.ext_id, permission.ext_id)
+
+      :denied = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :denied when negative grant rule is applied to the sop via subject ancestors" do
+      {:ok, subject_a} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject_p} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      :ok = Auth.add_child(subject_a.ext_id, subject_p.ext_id, Subject)
+      :ok = Auth.add_child(subject_p.ext_id, subject.ext_id, Subject)
+
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.deny_permission(subject_a.ext_id, object.ext_id, permission.ext_id)
+
+      :denied = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :denied when negative grant rule is applied to the sop via object ancestors" do
+      {:ok, object_a} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, object_p} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      :ok = Auth.add_child(object_a.ext_id, object_p.ext_id, Object)
+      :ok = Auth.add_child(object_p.ext_id, object.ext_id, Object)
+
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.deny_permission(subject.ext_id, object_a.ext_id, permission.ext_id)
+
+      :denied = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :denied when negative grant rule is applied to the sop via permission descendants" do
+      {:ok, permission_a} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      {:ok, permission_p} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      {:ok, permission} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      :ok = Auth.add_child(permission_a.ext_id, permission_p.ext_id, Permission)
+      :ok = Auth.add_child(permission_p.ext_id, permission.ext_id, Permission)
+
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, object} = Object.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.deny_permission(subject.ext_id, object.ext_id, permission.ext_id)
+
+      :denied = Auth.permission_granted?(subject.ext_id, object.ext_id, permission_a.ext_id)
+    end
+
+    test "returns :denied when negative grant rule is applied to the sop via subject ancestors despite more specific positive grant" do
+      {:ok, subject_a} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject_p} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      :ok = Auth.add_child(subject_a.ext_id, subject_p.ext_id, Subject)
+      :ok = Auth.add_child(subject_p.ext_id, subject.ext_id, Subject)
+
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.deny_permission(subject_a.ext_id, object.ext_id, permission.ext_id)
+      :ok = Auth.grant_permission(subject.ext_id, object.ext_id, permission.ext_id)
+
+      :denied = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :denied when negative grant rule is applied to the sop via object ancestors despite more specific positive grant" do
+      {:ok, object_a} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, object_p} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      :ok = Auth.add_child(object_a.ext_id, object_p.ext_id, Object)
+      :ok = Auth.add_child(object_p.ext_id, object.ext_id, Object)
+
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.deny_permission(subject.ext_id, object_a.ext_id, permission.ext_id)
+      :ok = Auth.grant_permission(subject.ext_id, object.ext_id, permission.ext_id)
+
+      :denied = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :denied when negative grant rule is applied to the sop via permission descendants despite more specific positive grant" do
+      {:ok, permission_a} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      {:ok, permission_p} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      {:ok, permission} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      :ok = Auth.add_child(permission_a.ext_id, permission_p.ext_id, Permission)
+      :ok = Auth.add_child(permission_p.ext_id, permission.ext_id, Permission)
+
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, object} = Object.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.deny_permission(subject.ext_id, object.ext_id, permission.ext_id)
+      :ok = Auth.grant_permission(subject.ext_id, object.ext_id, permission_a.ext_id)
+
+      :denied = Auth.permission_granted?(subject.ext_id, object.ext_id, permission_a.ext_id)
+    end
+
+    ################
+    ################
+
+    test "returns :granted when positive grant rule is applied to the sop" do
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.grant_permission(subject.ext_id, object.ext_id, permission.ext_id)
+
+      :granted = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :granted when positive grant rule is applied to the sop via subject ancestors" do
+      {:ok, subject_a} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject_p} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      :ok = Auth.add_child(subject_a.ext_id, subject_p.ext_id, Subject)
+      :ok = Auth.add_child(subject_p.ext_id, subject.ext_id, Subject)
+
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.grant_permission(subject_a.ext_id, object.ext_id, permission.ext_id)
+
+      :granted = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :granted when positive grant rule is applied to the sop via object ancestors" do
+      {:ok, object_a} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, object_p} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      :ok = Auth.add_child(object_a.ext_id, object_p.ext_id, Object)
+      :ok = Auth.add_child(object_p.ext_id, object.ext_id, Object)
+
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.grant_permission(subject.ext_id, object_a.ext_id, permission.ext_id)
+
+      :granted = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :granted when positive grant rule is applied to the sop via permission ancestors" do
+      {:ok, permission_a} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      {:ok, permission_p} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      {:ok, permission} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      :ok = Auth.add_child(permission_a.ext_id, permission_p.ext_id, Permission)
+      :ok = Auth.add_child(permission_p.ext_id, permission.ext_id, Permission)
+
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, object} = Object.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.grant_permission(subject.ext_id, object.ext_id, permission_a.ext_id)
+
+      :granted = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :granted when negative grant rule is applied to subject descendants" do
+      {:ok, subject_a} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject_p} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      :ok = Auth.add_child(subject_a.ext_id, subject_p.ext_id, Subject)
+      :ok = Auth.add_child(subject_p.ext_id, subject.ext_id, Subject)
+
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.grant_permission(subject_a.ext_id, object.ext_id, permission.ext_id)
+      :ok = Auth.deny_permission(subject.ext_id, object.ext_id, permission.ext_id)
+
+      :granted = Auth.permission_granted?(subject_a.ext_id, object.ext_id, permission.ext_id)
+    end
+
+    test "returns :granted when negative grant rule is applied to object descendants" do
+      {:ok, object_a} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, object_p} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      {:ok, object} = Object.new(UUID.generate(), "Object A") |> Repo.insert()
+      :ok = Auth.add_child(object_a.ext_id, object_p.ext_id, Object)
+      :ok = Auth.add_child(object_p.ext_id, object.ext_id, Object)
+
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, permission} = Permission.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.grant_permission(subject.ext_id, object_a.ext_id, permission.ext_id)
+      :ok = Auth.deny_permission(subject.ext_id, object.ext_id, permission.ext_id)
+
+      :granted = Auth.permission_granted?(subject.ext_id, object_a.ext_id, permission.ext_id)
+    end
+
+    test "returns :granted when negative grant rule is applied to permission ancestors" do
+      {:ok, permission_a} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      {:ok, permission_p} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      {:ok, permission} = Permission.new(UUID.generate(), "Permission A") |> Repo.insert()
+      :ok = Auth.add_child(permission_a.ext_id, permission_p.ext_id, Permission)
+      :ok = Auth.add_child(permission_p.ext_id, permission.ext_id, Permission)
+
+      {:ok, subject} = Subject.new(UUID.generate(), "Subject A") |> Repo.insert()
+      {:ok, object} = Object.new("edit", "edit stuff") |> Repo.insert()
+
+      :ok = Auth.deny_permission(subject.ext_id, object.ext_id, permission_a.ext_id)
+      :ok = Auth.grant_permission(subject.ext_id, object.ext_id, permission.ext_id)
+
+      :granted = Auth.permission_granted?(subject.ext_id, object.ext_id, permission.ext_id)
     end
   end
 end
